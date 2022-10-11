a115cf3d45e48d5996864d8fde2009c5c5d46dad
Gil <gilbertjvirgo@gmail.com>
2022-10-11 15:21:54 +0100

    Mid-update (Lucy needed the checklist to just be working)

---
+++ src/components/MiniButton.js
@@ -0,0 +1,9 @@
+import { Button } from "react-bootstrap";
+import styled from "styled-components";
+
+export default styled(Button)`
+	width: 25px;
+	height: 25px;
+	padding: 0;
+	line-height: 0;
+`;
